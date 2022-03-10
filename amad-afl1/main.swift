a595824d25d1c64d7b99c315f78b2ded2d526e72
3instein <reynaldi_kindarto@yahoo.com>
2022-03-10 22:35:46 +0700

    Initial Commit

--- amad-afl1/main.swift
+++ amad-afl1/main.swift
@@ -6,6 +6,67 @@
 //
 
 import Foundation
+import OrderedCollections
 
-print("Hello, World!")
+var shop = true; var buy = true
+
+var fnb: OrderedDictionary = [
+    "F03": "Gado-Gado",
+    "F02": "Chicken Satay",
+    "F01": "Nasi Padang",
+    "B02": "Mineral Water",
+    "B01": "Ice Tea"
+    ]
+
+var cart: OrderedDictionary = OrderedDictionary<String, Int>()
+
+while(shop){
+    print("============================")
+    print("    Point of Sales (PoS)")
+    print("============================\n")
+
+    print("Options:")
+    print("[1] Buy Food")
+    print("[2] Shopping Cart")
+    print("[x] Exit\n")
+
+    print("Your choice? ", terminator: ""); let choice = Int(readLine()!)
+
+    switch choice {
+    case 1:
+        print("Hi, we have " + String(fnb.count) + " Food and Beverage options for you!")
+        print("------------------------------------------------")
+        for(code, menu) in fnb{
+            print("[\(code)] \(menu)")
+        }
+        while(buy){
+            print("\nYour F&B choice? ", terminator: ""); let fnb_option = readLine()?.uppercased()
+            if(fnb_option?.caseInsensitiveCompare("q") == .orderedSame){
+                break
+            }
+            print("How many " + fnb["\(fnb_option ?? "")"]! + " you want to buy? ", terminator: "")
+            let amount = Int(readLine()!)
+            cart[fnb["\(fnb_option ?? "")"]!] = amount
+            print("Shopping Cart (\(cart.count) items):")
+            for item in cart{
+                print(String(item.value) + " " + String(item.key))
+            }
+        }
+    case 2:
+        if(cart.isEmpty){
+            print("Cart is empty")
+        } else {
+            print("Shopping Cart (\(cart.count) items):")
+            for item in cart{
+                print(String(item.value) + " " + String(item.key))
+            }
+        }
+    default:
+        shop = false
+    }
+}
+
+
+
+
 
